11d9dcfe115f9eb7a286f3248ab69f217cbbe055
Nathan Davidov <NathanDavidov9@gmail.com>
2025-08-16 04:45:48 -0400

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+    console.log("Mongoose connected");
+});
+
+const PORT = 8000;
+
+//middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Homepage ");
+});
+
+app.get("/users", (req, res) => {
+    res.send("Welcome to user page ");
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend server listening on port ${PORT}`);
+});
