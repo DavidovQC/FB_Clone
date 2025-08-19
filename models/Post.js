28be9f7d5a386380cd878a1ceb5b5a408baf8e39
Nathan Davidov <NathanDavidov9@gmail.com>
2025-08-19 05:43:44 -0400

    added post page and post schema

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+    {
+        userID: {
+            type: String,
+            required: true,
+        },
+
+        desc: {
+            type: String,
+            required: true,
+            max: 2000,
+        },
+
+        img: {
+            type: String,
+        },
+
+        likes: {
+            type: Array,
+            default: [],
+        },
+    },
+
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -55,8 +55,62 @@ router.delete("/:id", async (req, res) => {
 });
 
 //get a user
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).send(user.username);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+    try {
+        if (req.body.userID === req.params.id) {
+            return res.status(403).json("You cannot follow yourself!");
+        }
+        const userToFollow = await User.findById(req.params.id);
+        const user = await User.findById(req.body.userID);
+
+        if (user.following.includes(userToFollow._id)) {
+            return res.status(401).json("You already follow this user");
+        }
+
+        await user.updateOne({ $push: { following: userToFollow._id } });
+        await userToFollow.updateOne({ $push: { followers: user._id } });
+
+        return res
+            .status(200)
+            .send(`You are now following ${userToFollow.username}`);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 //unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+    try {
+        if (req.body.userID === req.params.id) {
+            return res.status(403).json("You cannot unfollow yourself!");
+        }
+        const userToUnfollow = await User.findById(req.params.id);
+        const user = await User.findById(req.body.userID);
+
+        if (!user.following.includes(userToUnfollow._id)) {
+            return res.status(401).json("You do not follow this user");
+        }
+
+        await user.updateOne({ $pull: { following: userToUnfollow._id } });
+        await userToUnfollow.updateOne({ $pull: { followers: user._id } });
+
+        return res
+            .status(200)
+            .send(`You are no longer following ${userToUnfollow.username}`);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
 
 router.post("/", (req, res) => {
     res.send("This is the user route");
